151a867ed824f17235c75a8c403b583b7438be91
Omar Ghalib <omarghalib@gmail.com>
2019-07-03 21:00:24 +0200

    9:00_3_7

--- Image_Processing_others.py
+++ Image_Processing_others.py
@@ -7,7 +7,7 @@ Created on Wed Jul  3 19:13:44 2019
 import cv2
 import numpy as np
 
-def process_image_others(filename, name):
+def process_image_erosion(filename, name):
     img = cv2.imread(filename)
     
     kernel = np.ones((3,3),np.uint8)
@@ -17,3 +17,14 @@ def process_image_others(filename, name):
     path = 'C:\\wamp64\\www\\Images_Storage\\Crop_Output\\' + name
     print(path)
     cv2.imwrite(path,erosion)
+    
+def process_image_dilation(filename, name):
+    img = cv2.imread(filename)
+    
+    kernel = np.ones((2,2),np.uint8)
+    #kernel_2 = np.ones((5,5),np.uint8)
+    dilation = cv2.dilate(img,kernel,iterations = 1)
+    #erosion = cv2.erode(img,kernel,iterations = 1)
+    path = 'C:\\wamp64\\www\\Images_Storage\\Crop_Output\\' + name
+    print(path)
+    cv2.imwrite(path,dilation)
--- Watch_files.py
+++ Watch_files.py
@@ -66,20 +66,41 @@ class Handler(FileSystemEventHandler):
             file_path_num = 'C:\\wamp64\\www\\Images_Storage\\Crop_Output\\cropped_N.jpg'
             Processed_num = "C:\\wamp64\\www\\Images_Storage\\Crop_Output\\P_num.png"
             Processed_name = "C:\\wamp64\\www\\Images_Storage\\Crop_Output\\P_name.png"
-            crop(image, (200, 20, 549, 133), file_path_name)
+            Processed_name_2 = "C:\\wamp64\\www\\Images_Storage\\Crop_Output\\P_name_2.png"
+            Processed_name_3 = "C:\\wamp64\\www\\Images_Storage\\Crop_Output\\P_name_3.png"
+            crop(image, (0, 20, 549, 133), file_path_name)
             crop(image, (0, 133, 549, 250), file_path_add)
             crop(image, (0, 261, 549, 380), file_path_num)
             print("Done Cropping")
             process_image(file_path_num,'P_num.png')
-            process_image_others(file_path_name,'P_name.png')
+            process_image_erosion(file_path_name,'P_name.png')
+            process_image_dilation(file_path_name,'P_name_2.png')
+            process_image(file_path_name,'P_name_3.png')
+            
             n1,n2 = OCR_Req_name(Processed_name)
-            name = n1 + n2
-            print("with I.P.:" + name)
+            name_1 = n1 + n2
+            print("with erosion:" + name_1)
             
-            #n1,n2 = OCR_Req_name(file_path_name)
-            #name = n1 + n2
-            #print("without bluring:" + name)
+            n1,n2 = OCR_Req_name(file_path_name)
+            name_2 = n1 + n2
+            print("without I.P:" + name_2)
             
+            n1,n2 = OCR_Req_name(Processed_name_3)
+            name_3 = n1 + n2
+            print("with blurring:" + name_3)
+            
+            
+            print( "max:" + max([name_1, name_2, name_3], key=len))
+            names = [name_1, name_2, name_3]
+            max_name = max(names, key=len)
+            if len(max_name) < 40:
+                name = max_name
+            else: 
+                names.remove(max_name)
+                if len(max(names, key=len)) < 40 :
+                    name = max(names, key=len)
+                else:
+                    name = min(names, key=len)
             #num_OCR_str = " "
             num_OCR = OCR_Nums(Processed_num)
             #num_OCR_str = num_OCR
